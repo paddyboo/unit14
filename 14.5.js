fba53d4767ff2f79ef5e66c1f5b7cb41bc4229c1
paddyboo <150559233+paddyboo@users.noreply.github.com>
2024-03-03 20:08:22 -0800

    Add files via upload

---
+++ 14.5.js
@@ -0,0 +1,74 @@
+  /* Same keys and value*/
+  function createInstructor(firstName, lastName){
+    return {
+      firstName: firstName,
+      lastName: lastName
+    }
+  }
+
+  /* Write an ES2015 Version */
+
+  function createInstructor (firstName, lastName){
+    return {firstName, lastName}
+  }
+
+
+/* Computed Property Names */
+ var favoriteNumber = 42;
+
+ var instructor = {
+  firstName: "Colt"
+ }
+
+ instructor[favoriteNumber] = "That is my favorite!"
+
+ /* Write an ES2015 Version */
+
+ let favoriteNumber=42;
+ onst instructor= {
+    firstName: "Colt",
+    [favoriteNumber]:"That is my favorite!"
+ }
+
+
+/* Object Methods*/
+ var instructor = {
+    firstName: "Colt",
+    sayHi: function(){
+      return "Hi!";
+    },
+    sayBye: function(){
+      return this.firstName + " says bye!";
+    }
+  }
+
+  /* Write an ES2015 Version */
+ const instructor={
+    firstName:"Colt",
+    sayHi() {
+        return "Hi!";
+    }
+    sayBye(){
+        return this.firstName+"says bye!";
+    }
+ }
+
+
+/*createAnimal function*/
+
+  const d = createAnimal("dog", "bark", "Woooof!")
+  // {species: "dog", bark: ƒ}
+  d.bark()  //"Woooof!"
+  
+  const s = createAnimal("sheep", "bleet", "BAAAAaaaa")
+  // {species: "sheep", bleet: ƒ}
+  s.bleet() //"BAAAAaaaa"
+
+
+  function createAnimal(species,verb,noise) {
+    return {
+        species, [verb](){
+            return noise;
+        }
+    }
+  }
